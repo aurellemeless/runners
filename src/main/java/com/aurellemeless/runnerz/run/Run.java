0f7c9661afd67f7d9c530dc67fa61dd373656f9a
Aurelle Meless <aurelle.meless@gmail.com>
2025-01-29 08:48:10 +0100

    using real DBMS Postgres by docker

--- src/main/java/com/aurellemeless/runnerz/run/Run.java
+++ src/main/java/com/aurellemeless/runnerz/run/Run.java
@@ -2,15 +2,22 @@ package com.aurellemeless.runnerz.run;
 
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.Positive;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.Version;
 
 import java.time.LocalDateTime;
 
-public record Run(Integer id,
-                  @NotEmpty
-                  String title,
-                  LocalDateTime startedOn,
-                  LocalDateTime completedOn,
-                  @Positive
-                  Integer miles,
-                  Location location) {
+public record Run(
+        @Id
+        Integer id,
+        @NotEmpty
+        String title,
+        LocalDateTime startedOn,
+        LocalDateTime completedOn,
+        @Positive
+        Integer miles,
+        Location location,
+        @Version
+        Integer version
+) {
 }
